3de03a190e92f17d271fe6310ad09ac2695334a9
MOHD YUSUF <mohdyusufr@gmail.com>
2024-11-23 21:17:58 +0530

    Fix out for delivery problem

--- lib/screens/admin/admin_dashboard_screen.dart
+++ lib/screens/admin/admin_dashboard_screen.dart
@@ -810,7 +810,7 @@ class _AdminDashboardScreenState extends State<AdminDashboardScreen> {
         return Colors.orange;
       case 'PROCESSING':
         return Colors.blue;
-      case 'OUT FOR DELIVERY':
+      case 'OUTFORDELIVERY':
         return Colors.purple;
       case 'DELIVERED':
         return Colors.green;
