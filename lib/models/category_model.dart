ec4e3369dd78006f6560beecbad38f11dcb77462
MOHD YUSUF <mohdyusufr@gmail.com>
2024-12-17 14:54:50 +0530

    Create category model

---
+++ lib/models/category_model.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class CategoryModel {
+  final String id;
+  final String name;
+  final String imageUrl;
+
+  CategoryModel({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+  });
+
+  factory CategoryModel.fromMap(Map<String, dynamic> map, String id) {
+    return CategoryModel(
+      id: id,
+      name: map['name'] ?? '',
+      imageUrl: map['imageUrl'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'imageUrl': imageUrl,
+    };
+  }
+
+  CategoryModel copyWith({
+    String? id,
+    String? name,
+    String? imageUrl,
+  }) {
+    return CategoryModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      imageUrl: imageUrl ?? this.imageUrl,
+    );
+  }
+}
