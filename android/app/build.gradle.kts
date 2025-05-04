d179aa4e2db1439578ee8cd837c9c8b93105cf91
MOHD YUSUF <mohdyusufr@gmail.com>
2025-05-04 07:48:12 +0530

    Update minimum version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         applicationId = "com.example.smart_kirana"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 22
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
