5b0b3a3d6fe4194f249dfa35d300386abcb524bc
MOHD YUSUF <mohdyusufr@gmail.com>
2025-05-27 18:39:43 +0530

    Update built files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,9 @@ dependencies {
 android {
     namespace = "com.example.smart_kirana"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+
+    // 🔧 Set correct NDK version
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -37,10 +39,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.smart_kirana"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 22
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -49,8 +48,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
